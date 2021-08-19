5a77844d80edced087c8b487967e7a1c94123046
gehatz <georgehatzi7@gmail.com>
2021-08-19 19:23:03 -0400

    Implement React State to set and iterate through courses and display them in the Courses component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from './common/Header'
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
-
+import CoursesPage from './CoursesPage';
 function App(){
+
     function getPage(){
         const route = window.location.pathname;
         if(route === "/about") return <AboutPage />;
+        if(route === "/courses") return <CoursesPage />;
+
         return <HomePage />;
     }
     
